127db81a1ef558be12d29742d802335b36d3f789
Rohit_Patel_IIITR <127647959+rohit-iiitr@users.noreply.github.com>
2025-09-04 20:35:36 +0530

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,11 @@ dotenv.config();
 connectDB();
 // morgan.config();
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: "https://my-shop-five-eta.vercel.app", // frontend ka domain
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 app.use(express.json());
 app.use(morgan("dev"));
 app.use("/api/v1/auth", authRoutes);
